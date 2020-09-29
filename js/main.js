dc7fef8e75a5d22dad08a0d07e43b41252e2cc71
TIIGRUS <>
2020-09-29 13:56:41 +0300

    init animaton to modal fix clases to open and close modal

--- js/main.js
+++ js/main.js
@@ -9,6 +9,18 @@
       $("#dialog").dialog({
         //   autoOpen: false,
         width: 640,
+        show: {
+            effect: "fade"
+        },
+        hide: {
+            effect: "fade"
+        },
+        open: function () {
+           $('.page').addClass('page_modal-opened');
+        },
+        close: function () {
+           $('.page').removeClass('page_modal-opened');
+        },
       });
 
       $(".product__btn").on("click", function () {
