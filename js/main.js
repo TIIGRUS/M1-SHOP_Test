e38447c3a7d78dfeb5b92f3f469ce498ad198c18
TIIGRUS <>
2020-09-29 13:18:21 +0300

    init modal, base structure modal, modal contnet

--- js/main.js
+++ js/main.js
@@ -5,4 +5,13 @@
               duration: 500
           }
       });
+
+      $("#dialog").dialog({
+        //   autoOpen: false,
+        width: 640,
+      });
+
+      $(".product__btn").on("click", function () {
+          $("#dialog").dialog("open");
+      });
   });
