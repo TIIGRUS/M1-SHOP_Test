8a29f4681c7a922b3ef040ffad0c13f915a10292
TIIGRUS <>
2020-09-30 14:34:03 +0300

    smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,36 +1,50 @@
   $(function () {
     // init tabs
-      $("#tabs").tabs({
-          hide: {
-              effect: "fade",
-              duration: 500
-          }
-      });
+    $("#tabs").tabs({
+      hide: {
+        effect: "fade",
+        duration: 500
+      }
+    });
 
     //   init modal dialog
-      $("#dialog").dialog({
-        autoOpen: false,
-        width: 640,
-        show: {
-            effect: "fade"
-        },
-        hide: {
-            effect: "fade"
-        },
-        open: function () {
-           $('.page').addClass('page_modal-opened');
-        },
-        close: function () {
-           $('.page').removeClass('page_modal-opened');
-        },
-      });
+    $("#dialog").dialog({
+      autoOpen: false,
+      width: 640,
+      show: {
+        effect: "fade"
+      },
+      hide: {
+        effect: "fade"
+      },
+      open: function () {
+        $('.page').addClass('page_modal-opened');
+      },
+      close: function () {
+        $('.page').removeClass('page_modal-opened');
+      },
+    });
 
-      // click and open modal
-      $(".product__btn").on("click", function () {
-          $("#dialog").dialog("open");
-      });
+    // click and open modal
+    $(".product__btn").on("click", function () {
+      $("#dialog").dialog("open");
+    });
 
-      // custom select
-      $("#speed").selectmenu();
-      
+    // custom select
+    $("#speed").selectmenu();
+
+    // smooth scrolling 
+    $('a').on('click', function (event) {
+      if (this.hash !== '') {
+        event.preventDefault();
+
+        let hash = this.hash;
+
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function () {
+          window.location.hash = hash;
+        });
+      }
+    })
   });
